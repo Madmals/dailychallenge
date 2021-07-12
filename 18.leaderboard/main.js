f3a360c3329b9115e1431ceff75053659b78b16d
k@m@l <kabota000@gmail.com>
2021-07-12 23:45:00 +0100

    finish rating 5

--- 18.leaderboard/main.js
+++ 18.leaderboard/main.js
@@ -4,25 +4,32 @@ const card_area = document.querySelector(".card-area")
 
 const image_url = 'https://image.tmdb.org/t/p/w1280'
 
+
+
+
+
+
+
 const trending = async () => {
-	let res = await fetch("https://api.themoviedb.org/3/trending/movie/week?api_key=7d01ce9433d80be3efed5507c5508810")
-	let data = await res.json()
-	console.log(data)
-	show_movies(data.results)
+    let res = await fetch("https://api.themoviedb.org/3/trending/movie/week?api_key=7d01ce9433d80be3efed5507c5508810")
+    let data = await res.json()
+    console.log(data)
+    show_movies(data.results)
+    get_rating()
 
 
 }
 
 const show_movies = (movie) => {
 
-	card_area.innerHTML = ``
-	movie.forEach(mov => {
-		const { title, poster_path, vote_average, release_date } = mov
+    card_area.innerHTML = ``
+    movie.forEach(mov => {
+        const { title, poster_path, vote_average, release_date } = mov
 
-		card_movie = document.createElement('div')
-		card_movie.classList.add('card')
+        card_movie = document.createElement('div')
+        card_movie.classList.add('card')
 
-		card_movie.innerHTML = `
+        card_movie.innerHTML = `
 
                 <div class="p-2 number">1</div>
                 <div class="img-container">
@@ -37,9 +44,13 @@ const show_movies = (movie) => {
 
                         <p class="rel">${release_date}</p>
 
-                        <div class="stars-outer">
-			<div class="stars-inner"></div>
-			</div>
+                
+                        <span class="stars-outer">
+			<div class="stars-inner">
+            </div>
+
+			</span>
+            <div class="voter">${vote_average}</div>
 
                         </div>
 
@@ -57,10 +68,46 @@ const show_movies = (movie) => {
 		
 		`
 
-		card_area.appendChild(card_movie)
+        card_area.appendChild(card_movie)
 
-	})
+    })
 }
+
+
+
+
+const get_rating = () => {
+    const rating_total = 10
+
+    let vote_all = document.querySelectorAll(".voter")
+
+
+
+    vote_all.forEach(eachvote=>{
+
+    // // get percentage
+    const star_percentage = (Number(eachvote.textContent) / rating_total) * 100
+
+    // //rounded to 10 decimal
+    const star_percentage_rounded = `${Math.round(star_percentage / 10) * 10}%`
+
+
+
+    //set width of stars inner to percentage
+
+    let a = document.querySelectorAll(".stars-inner")
+
+    a.forEach((list) => {
+        list.style.width = star_percentage_rounded
+        console.log(star_percentage_rounded)
+    })
+
+    })
+
+
+}
+
+
 
 
 trending()
